a11ab4a1dcfdfbbfdf14444b2cff799d144c67ff
definitelyna <nguyentrongnamanh@gmail.com>
2024-09-16 15:44:16 +0700

    nvm not using stt anymore

--- backend/models/attendee.model.js
+++ backend/models/attendee.model.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose")
 
 const AttendeeSchema = mongoose.Schema({
-  _id: false,
-  stt: {
+  _id: {
     type: Number,
     required: [true, "Please enter stt"],
     unique: true,
